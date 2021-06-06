7f17117e991070d8ac1b624ffeda74f96689ed87
Ozeco-Mmem <open3050@naver.com>
2021-06-06 18:52:58 +0900

    Update MyCarInfo
    info설정값 유지되게 변경
    플러그 설정에 필터링 초기화하는 0번지값 추가

--- app/src/main/java/com/example/myapplication/MyCarInfoActivity.java
+++ app/src/main/java/com/example/myapplication/MyCarInfoActivity.java
@@ -17,14 +17,19 @@ public class MyCarInfoActivity extends AppCompatActivity{
 
     String[] plugs;
     MyCarInfo carInfo = MyCarInfo.getInstance();
+    TextView CarName = findViewById(R.id.MyCarNameInput);
+    TextView CarCompany = findViewById(R.id.MyCarCompanyInput);
+    TextView CarRange = findViewById(R.id.MyCarRangeInput);
+    Spinner plugSpinner = findViewById(R.id.MyCarPlugInput);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
+        refreshCarInfoSetting();
+
         //내 차 이름
-        TextView CarName = findViewById(R.id.MyCarNameInput);
         CarName.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -44,7 +49,6 @@ public class MyCarInfoActivity extends AppCompatActivity{
         });
 
         //내 차량 제조사
-        TextView CarCompany = findViewById(R.id.MyCarCompanyInput);
         CarCompany.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -64,7 +68,6 @@ public class MyCarInfoActivity extends AppCompatActivity{
         });
 
         //내 차량 주행 가능 거리
-        TextView CarRange = findViewById(R.id.MyCarRangeInput);
         CarRange.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -83,17 +86,15 @@ public class MyCarInfoActivity extends AppCompatActivity{
             }
         });
 
-
         //플러그 스피너 설정
         plugs = getResources().getStringArray(R.array.Charge_Plug);
-        Spinner plugSpinner = findViewById(R.id.MyCarPlugInput);
         ArrayAdapter<String> plugAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,plugs);
         plugAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         plugSpinner.setAdapter(plugAdapter);
         plugSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                carInfo.cpTp = position + 1;
+                carInfo.cpTp = position;
 
             }
 
@@ -103,5 +104,19 @@ public class MyCarInfoActivity extends AppCompatActivity{
             }
         });
 
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        refreshCarInfoSetting();
+    }
+
+    protected void refreshCarInfoSetting()
+    {
+        CarName.setText(carInfo.name);
+        CarCompany.setText(carInfo.Company);
+        CarRange.setText(carInfo.range);
+        plugSpinner.setSelection(carInfo.cpTp);
     }
 }
